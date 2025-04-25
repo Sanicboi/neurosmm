d67e7948083e214bfe2de60889930a2f81c58536
sanicboi <alexandrzezekalo@gmail.com>
2025-04-25 17:12:39 +0200

    termination

--- src/main.py
+++ src/main.py
@@ -50,6 +50,6 @@ def run_flask():
 
 
 if __name__ == "__main__":
-    flask_thread = threading.Thread(target=run_flask)
+    flask_thread = threading.Thread(target=run_flask, daemon=True)
     flask_thread.start()
     asyncio.run(run_bot())
