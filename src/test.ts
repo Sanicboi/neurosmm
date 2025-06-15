1cad2c274f4f154710afa666b3e7b6ae654929d6
sanicboi <alexandrzezekalo@gmail.com>
2025-06-15 11:08:43 +0200

    redone

--- src/test.ts
+++ src/test.ts
@@ -4,25 +4,9 @@ import path from "path";
 import ffmpeg from "fluent-ffmpeg";
 
 (async () => {
-  const buffer = await fs.readFile(path.join(process.cwd(), "input.png"));
 
-  const r = await new Promise<Buffer>((resolve, reject) => {
-    const inputStream = new Readable();
-    inputStream.push(buffer);
-    inputStream.push(null);
-    let chunks: Uint8Array[] = [];
-    const outputStream = new Writable({
-      write(chunk: Uint8Array, encoding, callback) {
-        chunks.push(chunk);
-        callback();
-      },
-    });
-
-    outputStream.on("finish", () => {
-      resolve(Buffer.concat(chunks));
-    });
-
-    ffmpeg(inputStream)
+  await new Promise((resolve, reject) => {
+    ffmpeg(path.join(process.cwd(), "input.png"))
       .videoFilters([
         {
           filter: "scale",
@@ -30,8 +14,8 @@ import ffmpeg from "fluent-ffmpeg";
         },
       ])
       .on("error", reject)
-      .pipe(outputStream, { end: true });
-  });
-
-  await fs.writeFile(path.join(process.cwd(), "output.png"), r);
+      .on('end', resolve)
+      .output(path.join(process.cwd(), "output.png"))
+      .run();
+  })
 })();
