e840b23642c932e1298348810a91eddd8292e588
sanicboi <alexandrzezekalo@gmail.com>
2025-07-07 18:50:50 +0300

    fixed fragments

--- src/routers/generation.ts
+++ src/routers/generation.ts
@@ -259,6 +259,7 @@ export default async (bot: TelegramBot) => {
         videos: {
           avatar: true,
           voice: true,
+          fragments: true
         },
       },
     });
--- src/test.ts
+++ src/test.ts
@@ -2,26 +2,24 @@ import { Readable, Writable } from "typeorm/platform/PlatformTools";
 import fs from "fs/promises";
 import path from "path";
 import ffmpeg from "fluent-ffmpeg";
+import axios from "axios";
+import 'dotenv/config';
 
 (async () => {
-  const videoPath = path.join(process.cwd(), 'input.mp4');
-  const imagePath = path.join(process.cwd(), 'output.png')
-  await new Promise((resolve, reject) => {
-    ffmpeg(videoPath)
-      .input(imagePath)
-      .complexFilter([
-        {
-          filter: "overlay",
-          options: {
-            x: "(main_w-overlay_w)/2",
-            y: "(main_h-overlay_h)/2",
-            enable: `between(t\\,${3}\\,${5})`,
-          },
-        },
-      ])
-      .output(videoPath.split('.').map((el, idx) => idx === 0 ? el + '1' : el).join('.'))
-      .on("error", reject)
-      .on("end", resolve)
-      .run();
-  });
+  const res = await axios.post('https://api.segmind.com/v1/seedance-v1-lite-text-to-video', 
+    {
+  "duration": 5,
+  "aspect_ratio": "16:9",
+  "prompt": "A whimsical underwater world thriving with marine life in kaleidoscope colors.",
+  "resolution": "720p",
+  "seed": 56698,
+  "camera_fixed": false
+}, {
+  headers: {
+    'x-api-key': process.env.SEGMIND_KEY
+  },
+  responseType: 'arraybuffer'
+}
+  );
+  await fs.writeFile('test.mp4', Buffer.from(res.data));
 })();
