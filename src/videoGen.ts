e4ccc7f71f830c49e8e0464539a6d9e3303d1c6a
sanicboi <alexandrzezekalo@gmail.com>
2025-07-23 19:05:01 +0100

    veo 3 is now seedance

--- src/videoGen.ts
+++ src/videoGen.ts
@@ -3,11 +3,13 @@ import axios, { AxiosResponse } from "axios"
 
 
 export const genVideo = async (prompt: string): Promise<Buffer> => {
-    const res: AxiosResponse<any> = await axios.post('https://api.segmind.com/v1/veo-3', {
+    const res: AxiosResponse<any> = await axios.post('https://api.segmind.com/v1/seedance-v1-lite-text-to-video', {
         aspect_ratio: '9:16',
         prompt,
-        seed: 0,
-        generate_audio: true
+        seed: Math.round(Math.random() * 10000),
+        duration: 5,
+        resolution: '720p',
+        camera_fixed: false
     }, {
         headers: {
             'x-api-key': process.env.SEGMIND_KEY
